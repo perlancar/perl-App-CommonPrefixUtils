25da379cec50747fe4d585e1f7808d2ca71daf5f
perlancar <perlancar@gmail.com>
2026-06-03 09:34:18 +0700

    Add CLIs: common-prefix, majority-prefix

--- lib/App/CommonPrefixUtils.pm
+++ lib/App/CommonPrefixUtils.pm
@@ -1,11 +1,55 @@
 package App::CommonPrefixUtils;
 
+use 5.010001;
 use strict;
+use warnings;
 
 # AUTHORITY
 # DATE
 # DIST
 # VERSION
+
+our %SPEC;
+
+$SPEC{common_prefix} = {
+    v => 1.1,
+    summary => 'Calculate common prefix from supplied strings',
+    args => {
+        strings => {
+            schema => ['array*', of=>'str*'],
+            cmdline_src => 'stdin_or_args',
+            pos => 0,
+            slurpy => 1,
+        },
+    },
+};
+sub common_prefix {
+    require String::CommonPrefix;
+
+    my %args = @_;
+
+    [200, "OK", String::CommonPrefix::common_prefix(@{ $args{strings} // [] })];
+}
+
+$SPEC{majority_prefix} = {
+    v => 1.1,
+    summary => 'Calculate majority prefix from supplied strings',
+    args => {
+        strings => {
+            schema => ['array*', of=>'str*'],
+            cmdline_src => 'stdin_or_args',
+            pos => 0,
+            slurpy => 1,
+        },
+    },
+};
+sub majority_prefix {
+    require String::CommonPrefix;
+
+    my %args = @_;
+
+    [200, "OK", String::CommonPrefix::majority_prefix(@{ $args{strings} // [] })];
+}
 
 1;
 # ABSTRACT: Utilities related to common prefix
@@ -14,7 +58,9 @@ use strict;
 
 =head1 SYNOPSIS
 
-See the included scripts.
+See the included scripts:
+
+# INSERT_EXECS_LIST
 
 
 =head1 DESCRIPTION
